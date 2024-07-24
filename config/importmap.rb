f37e5165c95860d6c1432a5052e4a1f58fa425f4
jx-bamboo <jx-bamboo@outlook.com>
2024-07-24 23:40:35 +0800

    update importmap dir

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "bootstrap_bundle_min.js", preload: true
+pin "bootstrap", to: "/javascripts/bootstrap_bundle_min.js", preload: true
+pin "three", to: "/javascripts/three_module_min.js", preload: true
+pin 'web3', to: "https://unpkg.com/web3@4.10.0/dist/web3.min.js"
