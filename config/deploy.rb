ff4c4cea875793735907299cc47a5a28e544ec1f
jx-bamboo <jx-bamboo@outlook.com>
2024-07-24 16:51:04 +0800

    update deploy.rb for github

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,9 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.19.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "zaat"
+set :repo_url, "git@github.com:jx-bamboo/zaat_pro.git"
+set :branch, "main"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
