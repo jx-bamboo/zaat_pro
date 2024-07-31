464f8a345c1d16ad0c5fed69470aad3d57e09f68
jx-bamboo <jx-bamboo@outlook.com>
2024-07-31 16:53:25 +0800

    change bootstrap js method

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "/javascripts/bootstrap.bundle.min.js", preload: true
+pin "popper", to: 'popper.js', preload: true
+pin "bootstrap", to: 'bootstrap.min.js', preload: true
 pin "three", to: "/javascripts/three.module.min.js", preload: true
 pin 'web3', to: "https://unpkg.com/web3@4.10.0/dist/web3.min.js"
